cbaaa2efd3701771c3c29101f879d4167d391c24
schorcht-ioer <123582538+schorcht-ioer@users.noreply.github.com>
2023-04-28 15:19:05 +0200

    Update mapshp.js

--- previewers/v1.3/js/mapshp.js
+++ previewers/v1.3/js/mapshp.js
@@ -28,17 +28,17 @@ function writeContentAndData(data, fileUrl, file, title, authors) {
 
 function handleZipFile(data){
     console.log('data');
-    console.log(data);
-    convertToLayer(str2ab(data));
-    //var reader = new FileReader();
-    //reader.onload = function(){
-    //    if (reader.readyState != 2 || reader.error){
-    //        return;
-    //    } else {
-    //        convertToLayer(reader.result);
-    //    }
-    //}
-    //reader.readAsArrayBuffer(data);
+    //console.log(data);
+    //convertToLayer(str2ab(data));
+    var reader = new FileReader();
+    reader.onload = function(){
+        if (reader.readyState != 2 || reader.error){
+            return;
+        } else {
+            convertToLayer(str2ab(reader.result));
+        }
+    }
+    reader.readAsText()(data);
 }
 
 function convertToLayer(buffer){
