0ea7c9518f385e9a5997247d6f4243dde6a960b3
schorcht-ioer <123582538+schorcht-ioer@users.noreply.github.com>
2023-04-28 15:55:08 +0200

    Update mapshp.js

--- previewers/v1.3/js/mapshp.js
+++ previewers/v1.3/js/mapshp.js
@@ -27,7 +27,7 @@ function writeContentAndData(data, fileUrl, file, title, authors) {
 }
 
 function handleZipFile(data){
-    console.log('data');
+    console.log(data);
     data.then(convertToLayer(str2ab(data)));
     //if (document.readyState == 'loading') {
     //  // still loading, wait for the event
