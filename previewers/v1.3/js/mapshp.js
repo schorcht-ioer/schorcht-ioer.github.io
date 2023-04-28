c124ce66df7ae6e0ae2b5642973a94c4bb7712b0
schorcht-ioer <123582538+schorcht-ioer@users.noreply.github.com>
2023-04-28 15:25:29 +0200

    Update mapshp.js

--- previewers/v1.3/js/mapshp.js
+++ previewers/v1.3/js/mapshp.js
@@ -38,7 +38,7 @@ function handleZipFile(data){
             convertToLayer(str2ab(reader.result));
         }
     }
-    reader.readAsText()(data);
+    reader.readAsText(data);
 }
 
 function convertToLayer(buffer){
