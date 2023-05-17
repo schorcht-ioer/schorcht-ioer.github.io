0fbdb7a0ae7d402f6af4d9138c5b3da03b0aeca9
schorcht-ioer <123582538+schorcht-ioer@users.noreply.github.com>
2023-05-17 18:24:09 +0200

    Update map.js

--- previewers/v1.3/js/map.js
+++ previewers/v1.3/js/map.js
@@ -12,29 +12,30 @@ function writeContent(fileUrl, file, title, authors) {
     
     // set file size limits
     const file_size_limit = 100; // in MB    
-
-	//check file size
-	const url_to_file_info = fileUrl.replace("access/data","").replace("file","files");   
-
+    
+    //check file size
+    const url_to_file_info = fileUrl.replace("access/data","").replace("file","files");   
+    
     $.getJSON(url_to_file_info, function( data ) {
-  		const file_size = data.data.dataFile.filesize/(1024**2);
-		
-    	if (file_size > file_size_limit){
-    		show_error(`The file is too big to be displayed (limit is ${file_size_limit.toString()} MB)`);
-    	}else{
-            $.getJSON(fileUrl, function( geoJsonData ) {
-                // initialize the map
-                var map = L.map('map').fitWorld();
-                
-                // enable spinner
-                var target = document.getElementById('map');
-                var spinner = new Spinner().spin(target);
+        const file_size = data.data.dataFile.filesize/(1024**2);
+        
+        if (file_size > file_size_limit){
+            show_error(`The file is too big to be displayed (limit is ${file_size_limit.toString()} MB)`);
+        }else{
+            // initialize the map
+            var map = L.map('map').fitWorld();
             
-                // load a tile layer
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
+            // enable spinner
+            var target = document.getElementById('map');
+            var spinner = new Spinner().spin(target);
             
+            // load a tile layer
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+            
+            // get geojson
+            $.getJSON(fileUrl, function( geoJsonData ) { 
                 // add data to map, including properties if set
                 var geoJson = L.geoJSON(geoJsonData, {
                     onEachFeature: function (feature, layer) {
@@ -51,10 +52,10 @@ function writeContent(fileUrl, file, title, authors) {
                 
                 // zoom to added features
                 map.fitBounds(geoJson.getBounds());    
-      	        
+                
                 // disable spinner
-      	        spinner.stop();                
+                spinner.stop();                
             })      
-    	}
+        }
     })
 }
