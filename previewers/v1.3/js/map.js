92507d0a74bfc63c637b101406e678bd91c59dca
schorcht-ioer <123582538+schorcht-ioer@users.noreply.github.com>
2023-05-17 18:17:26 +0200

    Update map.js

--- previewers/v1.3/js/map.js
+++ previewers/v1.3/js/map.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    startPreview(true);
+    startPreview(false);   
 });
     
 function translateBaseHtmlPage() {
@@ -7,34 +7,54 @@ function translateBaseHtmlPage() {
     $( '.mapPreviewText' ).text( mapPreviewText );
 }
 
-function writeContentAndData(data, fileUrl, file, title, authors) {
+function writeContent(fileUrl, file, title, authors) {
     addStandardPreviewHeader(file, title, authors);
+    
+    // set file size limits
+    const file_size_limit = 100; // in MB    
 
-    // convert string data to json
-    var geoJsonData = JSON.parse(data);
+	//check file size
+	const url_to_file_info = fileUrl.replace("access/data","").replace("file","files");   
 
-    // initialize the map
-    var map = L.map('map').fitWorld();
-
-    // load a tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // add data to map, including properties if set
-    var geoJson = L.geoJSON(geoJsonData, {
-        onEachFeature: function (feature, layer) {
-            if (feature.properties) {
-                var popupcontent = [];
-                for (var propName in feature.properties) {
-                    propValue = feature.properties[propName];
-                    popupcontent.push("<strong>" + propName + "</strong>: " + JSON.stringify(propValue, null, 2));
-                }
-                layer.bindPopup(popupcontent.join("<br />"));
-            }
-        }
-      }).addTo(map);
-
-    // zoom to added features
-    map.fitBounds(geoJson.getBounds());
+    $.getJSON(url_to_file_info, function( data ) {
+  		const file_size = data.data.dataFile.filesize/(1024**2);
+		
+    	if (file_size > file_size_limit){
+    		show_error(`The file is too big to be displayed (limit is ${file_size_limit.toString()} MB)`);
+    	}else{
+            $.getJSON(fileUrl, function( geoJsonData ) {
+                // initialize the map
+                var map = L.map('map').fitWorld();
+                
+                // enable spinner
+                var target = document.getElementById('map');
+                var spinner = new Spinner().spin(target);
+            
+                // load a tile layer
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+            
+                // add data to map, including properties if set
+                var geoJson = L.geoJSON(geoJsonData, {
+                    onEachFeature: function (feature, layer) {
+                        if (feature.properties) {
+                            var popupcontent = [];
+                            for (var propName in feature.properties) {
+                                propValue = feature.properties[propName];
+                                popupcontent.push("<strong>" + propName + "</strong>: " + JSON.stringify(propValue, null, 2));
+                            }
+                            layer.bindPopup(popupcontent.join("<br />"));
+                        }
+                    }
+                }).addTo(map);
+                
+                // zoom to added features
+                map.fitBounds(geoJson.getBounds());    
+      	        
+                // disable spinner
+      	        spinner.stop();                
+            })      
+    	}
+    })
 }
